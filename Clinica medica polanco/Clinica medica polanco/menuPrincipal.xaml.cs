3b42a75d5c644d691905a6283c250f42af94d812
Kevin Garcia <d.kevin0402@gmail.com>
2022-02-13 19:19:22 -0600

    Agregado el menu principal

---
+++ Clinica medica polanco/Clinica medica polanco/menuPrincipal.xaml.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Clinica_medica_polanco
+{
+    /// <summary>
+    /// Lógica de interacción para menuPrincipalNa.xaml
+    /// </summary>
+    public partial class menuPrincipal : Window
+    {
+        public menuPrincipal()
+        {
+            InitializeComponent();
+            this.Width = 1080;
+            this.Height = 620;
+        }
+
+      
+    }
+}
