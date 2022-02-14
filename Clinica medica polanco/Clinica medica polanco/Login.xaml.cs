14df251f2412bf147fcae0e1f88ff613a1af56f1
Leticia Díaz <diazleticia228@gmail.com>
2022-02-13 19:49:19 -0600

    Terminado login

--- Clinica medica polanco/Clinica medica polanco/Login.xaml.cs
+++ Clinica medica polanco/Clinica medica polanco/Login.xaml.cs
@@ -18,9 +18,9 @@ namespace Clinica_medica_polanco
     /// <summary>
     /// Interaction logic for MainWindow.xaml
     /// </summary>
-    public partial class MainWindow : Window
+    public partial class Login : Window
     {
-        public MainWindow()
+        public Login()
         {
             InitializeComponent();
         }
